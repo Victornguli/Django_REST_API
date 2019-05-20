938967ffad56e54a5f281e4b721043fd4c9fd728
Victor <victornjoseph@gmail.com>
2019-05-20 13:16:10 +0300

    First commit

---
+++ quickstart/views.py
@@ -0,0 +1,20 @@
+# from django.shortcuts import render
+# from django.contrib.auth.admin import User, Group
+# from rest_framework import viewsets
+# from quickstart.serializers import UserSerializer, GroupSerializer
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#         API Endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#         API Endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
