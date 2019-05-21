199c5804a649bee2bac34e188e93d39ddbfb5a61
Victor <victornjoseph@gmail.com>
2019-05-21 16:34:46 +0300

    Refactored views

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,20 +1,49 @@
-# from django.shortcuts import render
-# from django.contrib.auth.admin import User, Group
-# from rest_framework import viewsets
-# from quickstart.serializers import UserSerializer, GroupSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from quickstart.models import Snippet
+from quickstart.serializers import SnippetSerializer
+
+@api_view(["GET","POST"])
+def snippet_list(request, format="json"):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        print (serializer)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = SnippetSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#         API Endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
 
-# class GroupViewSet(viewsets.ModelViewSet):
-#     """
-#         API Endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -2,14 +2,17 @@
 from django.urls import path, include
 from rest_framework import routers
 from quickstart import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 # router = routers.DefaultRouter()
 # router.register(r"users", views.UserViewSet)
 # router.register(r"groups", views.GroupViewSet)
 
 
 urlpatterns = [
-    # path(),
+    path("snippets/", views.snippet_list),
+    path("snippets/<int:pk>", views.snippet_detail),
     # path('', include(router.urls)),
     # path("api-auth/", include("rest_framework.urls"), name="rest_framework")
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
